52878d85d202ffa695bafd471cc48ca0f3b2bf64
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-04-17 17:59:10 +0430

    Added custom user model, manager and migrations

---
+++ accounts/managers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """Manager for custom user model"""
+
+    def create_user(self, username, email, password=None):
+        """Create a new user"""
+        if not email:
+            raise ValueError("User must have an email adress")
+        
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_super_user(self, username, email, password):
+        """Create new superuser"""
+        user = self.create_user(username=username, email=email, password=password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.core.mail import send_mail
 
-# Create your models here.
+from .managers import UserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """Custom user model"""
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+    )
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
+
+    objects = UserManager() 
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    def __str__(self) -> str:
+        return self.username
