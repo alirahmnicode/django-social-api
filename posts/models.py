dfb3f51a9ab9e73de682383b9b2d54000c87ecd7
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-04-24 19:12:45 +0430

    create post and comment model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    """Post Model"""
+
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.Case, related_name="posts")
+    image = models.ImageField(upload_to="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.title[:20]} -> {self.user.username}"
+
+
+class Comment(models.Model):
+    """Comments for each post"""
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author_name = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.post.title[:20]} -> {self.content[:20]}"
