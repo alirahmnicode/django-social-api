bfc12d24a28d8ec59d7274c69d719a7fc125e1fa
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-05-02 20:36:50 +0430

    create url

--- posts/urls.py
+++ posts/urls.py
@@ -1,3 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+
+router = routers.DefaultRouter()
+router.register('comments', views.CommentViewSet)
+router.register('', views.PostViewSet)
+
+
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
