ed2141deeb84e5846223fa4598981f28a7a1d326
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-05-02 20:36:18 +0430

    update post serilizer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,16 +1,34 @@
 from rest_framework import serializers
+
+from accounts.serializers import UserSerializer
 from .models import Post, Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ('id', 'author_name', 'content', 'created_at')
+        fields = ("id", "author_name", "content", "created_at")
 
-        
+
 class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
+    user = UserSerializer(required=False)
+
     class Meta:
         model = Post
-        fields = ('id', 'title', 'content', 'image', 'created_at', 'comments')
+        fields = ("id", "title", "content", "image", "user", "created_at", "comments")
+
+    def create(self, validated_data):
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
+
+        post = Post.objects.create(
+            title=validated_data.get("title"),
+            content=validated_data.get("content"),
+            user=user,
+            image=validated_data.get("image"),
+        )
+        return post
 
