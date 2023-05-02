53e94cd5b0259214da377849c24bad2cd018d4fc
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-05-02 12:15:59 +0430

    write user and profile views

--- accounts/views.py
+++ accounts/views.py
@@ -1,32 +1,48 @@
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 
 from accounts.models import CustomUser, Profile
-from accounts.serializers import UserSerializer, ProfileSerializer
-from accounts.permissions import UpdateOwnAccount
+from accounts import serializers
+from accounts.permissions import UpdateOwnAccount, OwnAccount
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """Handle creating and updating users"""
-    serializer_class = UserSerializer
+
+    serializer_class = serializers.UserSerializer
     queryset = CustomUser.objects.all()
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (UpdateOwnAccount,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ("username", "email")
 
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action in ["retrieve", "destroy", "update", "partial_update"]:
+            permission_classes = [IsAuthenticated, OwnAccount]
+        elif self.action == "create":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
 
-class UserLoginApiView(ObtainAuthToken):
-    """Handle creating user authentication token"""
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    def get_serializer_class(self):
+        if self.action in ["retrieve", "destroy", "update", "partial_update"]:
+            # return user email field
+            self.serializer_class = serializers.UserOwnAccountSerializer
+        elif self.action == "create":
+            # use UserRegisterationSerializer for return token
+            self.serializer_class = serializers.UserRegisterationSerializer
+        else:
+            # only username field
+            self.serializer_class = serializers.UserSerializer
+        return super().get_serializer_class()
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     """Handle CURD profile"""
-    serializer_class = ProfileSerializer
+
+    serializer_class = serializers.ProfileSerializer
     queryset = Profile.objects.all()
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (UpdateOwnAccount,)
