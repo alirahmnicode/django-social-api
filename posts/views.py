88de577bd68a1d4a0a137008c17fb664704db0b1
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-05-02 20:55:17 +0430

    compelete post viewset

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment
+from custom_permission.permissions import OwnObject
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated]
+        if self.action in ["update", "destroy", "partial_update"]:
+            permission_classes = [IsAuthenticated, OwnObject]
+        return [permission() for permission in permission_classes]
